b9e08e84ab38a5ea94ba3248ac2f6e0ddfcd3af4
Opemipo Ashiru <opemipo.ashiru@pau.edu.ng>
2024-06-13 15:11:50 +0100

    (my commit)

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+ 
+ import { NestExpressApplication } from '@nestjs/platform-express';
+ 
+ import { join } from 'path';
+
+ import * as nunjucks from 'nunjucks';
+ async function bootstrap() {
+
+ const app = await NestFactory.create<NestExpressApplication>(
+ AppModule,
+ );
+
+const express = app.getHttpAdapter().getInstance();
+
+const views = join(__dirname, '..', 'views'); 
+
+nunjucks.configure(views, { express }); 
+
+await app.listen(3000);
+}
+bootstrap();
